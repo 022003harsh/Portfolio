9f9dc78d7f9cba31394745cc6cfe6d142ca25157
Harsh <harshku2222@gmail.com>
2024-03-22 04:12:35 +0530

    error fix

--- src/components/core/About.tsx
+++ src/components/core/About.tsx
@@ -5,9 +5,10 @@ const About = () => {
   return (
     <div className="w-full h-full rounded-3xl p-6 gap-4 xl:gap-10 flex flex-col justify-between bg-gradient-to-r from-slate-900 to-slate-800 overflow-hidden">
         <h2 className='text-white sm:text-lg mb-3 text-base font-semibold font-edu-sa'>About me</h2>
-      <div className="cursor-default">
-         I've contributed to diverse projects, showcasing leadership and effective communication skills.
-      </div>
+        <div className="cursor-default">
+            I&apos;ve contributed to diverse projects, showcasing leadership and effective communication skills.
+        </div>
+
       <div>
         {/* <p className='text-[#817e7e] text-xs sm:text-base font-medium'>Languages</p> */}
         {/* <h2 className='text-[#817e7e] text-sm sm:text-lg font-bold font-edu-sa'>About me</h2> */}
--- src/components/core/LetsConnect.tsx
+++ src/components/core/LetsConnect.tsx
@@ -19,7 +19,7 @@ const LetsConnect = () => {
       <Link href="">
           <GiPaperArrow className='absolute right-10 bottom-8 md:bottom-14 cursor-pointer group-hover:text-white text-[#817e7e] text-4xl -rotate-[35deg] transition-all duration-500'/>
       </Link>
-          <div className='mt-[40px] sm:mt-[70px] '>Let's</div>
+          <div className='mt-[40px] sm:mt-[70px] '>Let&apos;s</div>
           <div className='flex'>
             <div className=''>work</div>
             <div className='ml-[14px] h-[60px] bg-gradient-to-r from-green-600 to-white bg-clip-text text-transparent'> together.</div>
