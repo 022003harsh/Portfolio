b223b69e1c47107861c9bb3a695c22f580a96d89
Harsh <harshku2222@gmail.com>
2024-03-29 15:58:59 +0530

    bg color changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import Footer from "@/components/shared/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen max-w-full bg-black flex flex-col text-white px-2 relative">
-      <div className="w-[100vw] h-[100vh] fixed z-0 top-0 left-0" style={{
+      <div className="w-[100vw] h-[100vh] fixed z-0 top-0 left-0" 
+    //   style={{
+    //   background:
+    //     'rgb(116,21,21); background: linear-gradient(199deg, rgba(116,21,21,1) 0%, rgba(148,100,115,0) 50%, rgba(0,104,125,1) 100%)'
+    // }}
+      style={{
       background:
-        'rgb(116,21,21); background: linear-gradient(199deg, rgba(116,21,21,1) 0%, rgba(148,100,115,0) 50%, rgba(0,104,125,1) 100%)'
-    }}></div>
+        'rgb(116,41,81); background: linear-gradient(199deg, rgba(16,1,55,1) 0%, rgba(18,10,115,0) 50%, rgba(0,14,125,1) 100%)'
+    }}
+    ></div>
       <div className="max-w-7xl w-full mx-auto flex-grow flex flex-col gap-y-5 z-10">
         <Navbar />
         <HomePage />
