56cb57e7a684856b5d355249468f6366059ab035
Harsh <harshku2222@gmail.com>
2024-04-05 18:31:35 +0530

    final changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Roboto } from 'next/font/google'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] })
@@ -17,8 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Edu+SA+Beginner:wght@400..700&family=Lobster&display=swap" rel="stylesheet" />
+      </Head>
       <body className={inter.className}>{children}</body>
-      <link href="https://fonts.googleapis.com/css2?family=Edu+SA+Beginner:wght@400..700&family=Lobster&display=swap" rel="stylesheet"></link>
     </html>
   )
 }
--- src/components/core/About.tsx
+++ src/components/core/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="w-full h-full rounded-3xl p-6 gap-4 flex flex-col justify-between bg-gradient-to-r from-slate-900 to-slate-800 overflow-hidden">
         <h2 className='text-white sm:text-lg text-base font-bold font-edu-sa'>About me</h2>
-        <div className="cursor-default text-stone-400 font-edu-sa">
+        <div className="cursor-default text-stone-400 text-md md:text-lg font-edu-sa">
             I&apos;ve contributed to diverse projects, showcasing leadership and effective communication skills. Working as a
             Frontend developer
             also worked as a
